e877f4b3b8184d40ff3423240863846b1d093ddb
ateef <ateefksa@gmail.com>
2024-07-13 19:10:19 +0530

    third day

---
+++ leftRotateByKelements.py
@@ -0,0 +1,26 @@
+# Rotate array by K elements
+
+def reverse(arr, start, end):
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+
+def rotate(arr, k):
+    n = len(arr)
+    k = k % n
+
+    # Reverse the first k elements
+    reverse(arr, 0, k - 1)
+
+    # Reverse the next elements
+    reverse(arr, k, n - 1)
+
+    # Reverse the entire array
+    reverse(arr, 0, n - 1)
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5]
+    k = 2
+    rotate(arr, k)
+    print(f"After rotating the elements to left by {k}: {arr}")
