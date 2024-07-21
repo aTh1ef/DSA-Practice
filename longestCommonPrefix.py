bf75478bd78ee7bde4e0d48b9d4952ff66d2bda0
aTh1ef <ateefksa@gmail.com>
2024-07-21 21:25:11 +0530

    day 9

---
+++ longestCommonPrefix.py
@@ -0,0 +1,26 @@
+#Find Longest Common Prefix (string problem 4)
+from typing import List
+def longestCommonPrefix(strs: List[str]) -> str:
+    if not strs:
+        return ""
+
+    min_length = float('inf')
+    for s in strs:
+        if len(s) < min_length:
+            min_length = len(s)
+
+    i=0
+    while i < min_length:
+        for s in strs:
+            if s[i] != strs[0][i]:
+                return s[:i]
+        i += 1
+
+    return s[:i]
+
+if __name__ == "__main__":
+    strs = ['flower','flow','flight']
+    result = longestCommonPrefix(strs)
+    print("The longest common prefix is:", result)
+
+
