6e9b5b74543cbc7d5ee5b6a41af9fe2cdc0c90f6
aTh1ef <ateefksa@gmail.com>
2025-02-26 15:30:06 +0530

    Revision of first 20 string questions

--- duplicatesRev.py
+++ duplicatesRev.py
@@ -1,22 +1,10 @@
-def remove(s: str) -> str:
-    result = []
-    open = 0
+def wordsreverse(s: str) -> str:
+    words = s.split()
+    reversew = words[::-1]
 
-    for char in s:
-        if char == '(':
-            if open > 0:
-                result.append(char)
-            open += 1
-
-        elif char == ')':
-            open -= 1
-            if open > 0:
-                result.append(char)
-
-    answer = ''.join(result)
-    return answer
+    return ' '.join(reversew)
 
 if __name__ == "__main__":
-    s = "(()())(())"
-    answer1 = remove(s)
-    print("The string after removing the outermost parentheses is:", answer1)
+    s = "my name is japanese"
+    answer = wordsreverse(s)
+    print(answer)
