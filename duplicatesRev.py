e813e7d2aa0dfa0daca3f1f8ac7dad3deb17a47a
aTh1ef <ateefksa@gmail.com>
2025-02-26 23:44:48 +0530

    Revision of first 13 string questions

--- duplicatesRev.py
+++ duplicatesRev.py
@@ -1,15 +1,23 @@
-def largestOdd(s: str) -> str:
-    n = len(s)
-    largestoddnum = -1
-    for i in range(0,n):
-        if int(s[i]) % 2 != 0:
-            largestoddnum = i
+def iso(s1:str, s2:str) -> str:
+    maps1s2, maps2s1 = {}, {}
 
-    result = s[:largestoddnum + 1]
-    return result
+    if len(s1) != len(s2):
+        return False
 
+    for i in range(len(s1)):
+        c1, c2 = s1[i], s2[i]
+
+        if ((c1 in maps1s2 and maps1s2[c1] !=c2) or
+            (c2 in maps2s1 and maps2s1[c2] !=c1)):
+
+         return False
+
+        maps1s2[c1] =c2
+        maps2s1[c2] =c1
+    return True
 
 if __name__ == "__main__":
-    s = '42068'
-    ans = largestOdd(s)
-    print(ans)
+    s1 = "boo"
+    s2 = "bar"
+    result = iso(s1, s2)
+    print("Are the two strings isomorphic True or False -> ", result)
