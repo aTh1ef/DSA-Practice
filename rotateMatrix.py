6f8534756064062f3dc59e25fe45bab6b7f58f06
aTh1ef <ateefksa@gmail.com>
2025-03-28 22:23:46 +0530

    neetcode 43

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -1,5 +1,5 @@
 #Write a function to rotate a given n×n matrix by 90 degrees clockwise in-place without using extra space.
-
+#neetcode 43
 def rotateMatrix(matrix):
     n = len(matrix)
 
@@ -24,3 +24,23 @@ if __name__ == "__main__":
     print("Matrix after rotation:")
     for row in matrix:
         print(row)
+
+#alternate solution
+
+# solution with manual reverse function
+# class Solution:
+#     def rotate(self, matrix):
+#         n = len(matrix)
+#         for i in range(n):
+#             for j in range(i, n):
+#                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+#         for i in range(n):
+#             self.manual_reverse(matrix[i])
+
+#     def manual_reverse(self, lst):
+#         left = 0
+#         right = len(lst) - 1
+#         while left < right:
+#             lst[left], lst[right] = lst[right], lst[left]
+#             left += 1
+#             right -= 1
